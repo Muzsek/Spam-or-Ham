4bda259cbeefd70c57f001b292832bc4286078d6
Muzsek <147140150+Muzsek@users.noreply.github.com>
2025-08-17 16:57:10 +0200

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,13 @@
+def Talal(nums,target):
+    answer=[]
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                answer.append(i)
+                answer.append(j)
+
+    print(answer)
+
+nums = [2,7,11,15]
+target = 9
+Talal(nums,target)
